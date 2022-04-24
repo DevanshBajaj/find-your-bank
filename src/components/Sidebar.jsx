aafaa8e348b55223c493d0e32d29a870955339dc
Devansh Bajaj <devansh21db.db@gmail.com>
2022-04-24 15:55:38 +0530

    feat: add footer

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,6 +8,7 @@ import {
 	List,
 	Toolbar,
 	Typography,
+	styled,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
@@ -17,6 +18,16 @@ import Banks from "./Banks";
 import Favourites from "./Favourites";
 import BankDetails from "./BankDetails";
 import Home from "./Home";
+
+const FooterContainer = styled("div")`
+	width: 100%;
+	height: 4rem;
+	position: absolute;
+	bottom: 0px;
+	left: 0px;
+	text-align: center;
+	font-size: 1.4rem;
+`;
 
 const drawerWidth = 240;
 function Sidebar(props) {
@@ -87,129 +98,150 @@ function Sidebar(props) {
 		window !== undefined ? () => window().document.body : undefined;
 
 	return (
-		<Box
-			sx={{
-				display: "flex",
-				justifyContent: "space-around",
-				alignItems: "center",
-			}}
-		>
-			<CssBaseline />
-			<AppBar
-				position="fixed"
+		<>
+			<Box
 				sx={{
-					width: { sm: `calc(100% - ${drawerWidth}px)` },
-					ml: { sm: `${drawerWidth}px` },
-					backgroundColor: "background.paper",
-					boxShadow: "3px 6px 24px rgba(0,0,0,0.1) ",
+					display: "flex",
+					justifyContent: "space-around",
+					alignItems: "center",
 				}}
 			>
-				<Toolbar
+				<CssBaseline />
+				<AppBar
+					position="fixed"
 					sx={{
-						display: "flex",
-						justifyContent: "space-between",
-						px: "2rem",
+						width: { sm: `calc(100% - ${drawerWidth}px)` },
+						ml: { sm: `${drawerWidth}px` },
+						backgroundColor: "background.paper",
+						boxShadow: "3px 6px 24px rgba(0,0,0,0.1) ",
 					}}
 				>
-					<IconButton
-						color="inherit"
-						aria-label="open drawer"
-						edge="start"
-						onClick={handleDrawerToggle}
+					<Toolbar
 						sx={{
-							mr: 2,
-							display: { sm: "none" },
-							backgroundColor: "#26D7AB",
-							boxShadow: "3px 6px 24px rgba(0,0,0,0.1)",
+							display: "flex",
+							justifyContent: "space-between",
+							px: "2rem",
 						}}
 					>
-						<MenuIcon />
-					</IconButton>
-					<Typography variant="h6" color="text.primary" noWrap component="div">
-						{title}
-					</Typography>
-					<IconButton sx={{ ml: 1 }} onClick={props.colorMode} color="inherit">
-						{props.theme === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
-					</IconButton>
-				</Toolbar>
-			</AppBar>
-			<Box
-				component="nav"
-				sx={{
-					width: { sm: drawerWidth },
-					flexShrink: { sm: 0 },
-				}}
-				aria-label="mailbox folders"
-			>
-				{/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-				<Drawer
-					container={container}
-					variant="temporary"
-					open={mobileOpen}
-					onClose={handleDrawerToggle}
-					ModalProps={{
-						keepMounted: true, // Better open performance on mobile.
-					}}
+						<IconButton
+							color="inherit"
+							aria-label="open drawer"
+							edge="start"
+							onClick={handleDrawerToggle}
+							sx={{
+								mr: 2,
+								display: { sm: "none" },
+								backgroundColor: "#26D7AB",
+								boxShadow: "3px 6px 24px rgba(0,0,0,0.1)",
+							}}
+						>
+							<MenuIcon />
+						</IconButton>
+						<Typography
+							variant="h6"
+							color="text.primary"
+							noWrap
+							component="div"
+						>
+							{title}
+						</Typography>
+						<IconButton
+							sx={{ ml: 1 }}
+							onClick={props.colorMode}
+							color="inherit"
+						>
+							{props.theme === "dark" ? (
+								<Brightness7Icon />
+							) : (
+								<Brightness4Icon />
+							)}
+						</IconButton>
+					</Toolbar>
+				</AppBar>
+				<Box
+					component="nav"
 					sx={{
-						display: { xs: "block", sm: "none" },
-						"& .MuiDrawer-paper": {
-							boxSizing: "border-box",
-							backgroundColor: "#1e2232",
-							width: drawerWidth,
-							textAlign: "center",
-							display: "flex",
-							flexDirection: "row",
-							justifyContent: "center",
-							fontSize: "1.2rem",
-							gap: "2rem",
-						},
+						width: { sm: drawerWidth },
+						flexShrink: { sm: 0 },
+					}}
+					aria-label="mailbox folders"
+				>
+					{/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+					<Drawer
+						container={container}
+						variant="temporary"
+						open={mobileOpen}
+						onClose={handleDrawerToggle}
+						ModalProps={{
+							keepMounted: true, // Better open performance on mobile.
+						}}
+						sx={{
+							display: { xs: "block", sm: "none" },
+							"& .MuiDrawer-paper": {
+								boxSizing: "border-box",
+								backgroundColor: "#1e2232",
+								width: drawerWidth,
+								textAlign: "center",
+								display: "flex",
+								flexDirection: "row",
+								justifyContent: "center",
+								fontSize: "1.2rem",
+								gap: "2rem",
+							},
+						}}
+					>
+						{drawer}
+					</Drawer>
+					<Drawer
+						variant="permanent"
+						sx={{
+							display: { xs: "none", sm: "block" },
+							"& .MuiDrawer-paper": {
+								boxSizing: "border-box",
+								width: drawerWidth,
+								backgroundColor: "#1e2232",
+								textAlign: "center",
+								display: "flex",
+								flexDirection: "row",
+								justifyContent: "center",
+								borderRight: "1px solid #26d7ab",
+								fontSize: "1.6rem",
+								gap: "2rem",
+							},
+						}}
+						open
+					>
+						{drawer}
+					</Drawer>
+				</Box>
+				<Box
+					component="main"
+					sx={{
+						flexGrow: 1,
+						p: 6,
+						width: { sm: `calc(100% - ${drawerWidth}px)`, xs: "100%" },
 					}}
 				>
-					{drawer}
-				</Drawer>
-				<Drawer
-					variant="permanent"
-					sx={{
-						display: { xs: "none", sm: "block" },
-						"& .MuiDrawer-paper": {
-							boxSizing: "border-box",
-							width: drawerWidth,
-							backgroundColor: "#1e2232",
-							textAlign: "center",
-							display: "flex",
-							flexDirection: "row",
-							justifyContent: "center",
-							borderRight: "1px solid #26d7ab",
-							fontSize: "1.6rem",
-							gap: "2rem",
-						},
-					}}
-					open
-				>
-					{drawer}
-				</Drawer>
+					<Toolbar />
+					<Routes>
+						<Route exact={true} path="/" element={<Home />}></Route>
+						<Route exact={true} path="/all-banks" element={<Banks />}></Route>
+						<Route
+							exact={true}
+							path="/favorites"
+							element={<Favourites />}
+						></Route>
+						<Route path="/bank-details/:id" element={<BankDetails />} />
+					</Routes>
+				</Box>
 			</Box>
-			<Box
-				component="main"
-				sx={{
-					flexGrow: 1,
-					p: 6,
-					width: { sm: `calc(100% - ${drawerWidth}px)`, xs: "100%" },
-				}}
-			>
-				<Toolbar />
-				<Routes>
-					<Route exact={true} path="/" element={<Home />}></Route>
-					<Route exact={true} path="/all-banks" element={<Banks />}></Route>
-					<Route
-						exact={true}
-						path="/favorites"
-						element={<Favourites />}
-					></Route>
-					<Route path="/bank-details/:id" element={<BankDetails />} />
-				</Routes>
-			</Box>
-		</Box>
+			<FooterContainer>
+				Made with ❤ by{" "}
+				<a href="https://github.com/DevanshBajaj" style={{ color: "#26d7ab" }}>
+					Devansh Bajaj
+				</a>
+			</FooterContainer>
+		</>
 	);
 }
 
